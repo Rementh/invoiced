c9232c794c155424c1ab7322ec0d0cf0bb7806a9
Filip <talaga.filip@gmail.com>
2020-03-07 23:28:04 +0100

    Data model naming change

--- src/views/dashboard/models.ts
+++ src/views/dashboard/models.ts
@@ -1,12 +1,29 @@
-export enum ProductUnit {
-    Piece = 'szt.',
-    Hour = 'godz.',
-}
-
 export interface Product {
     name: string;
-    unit: ProductUnit;
+    unit: string;
     quantity: number;
-    netUnitPrice: number;
-    tax: number;
+    unitNetValue: number;
+    taxRate: number;
+}
+
+export interface Invoice {
+    products: {
+        no: number;
+        name: string;
+        unit: string;
+        quantity: number;
+        unitNetValue: number;
+        totalNetValue: number;
+        taxRate: number;
+        totalGrossValue: number;
+    }[];
+    taxRatesSummary: {
+        taxRate: number;
+        netValue: number;
+        taxValue: number;
+        grossValue: number;
+    }[];
+    totalNetValue: number;
+    totalTaxValue: number;
+    totalGrossValue: number;
 }

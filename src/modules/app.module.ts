4106eed53eb8d655e7963675b390187cd2523e86
Filip <talaga.filip@gmail.com>
2020-03-15 18:57:40 +0100

    Imported string-prototypes

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 /* Prototype */
 import '../utils/number-prototypes';
 import '../utils/array-prototypes';
+import '../utils/string-prototypes';
 
 /* Imports */
 import { BrowserModule } from '@angular/platform-browser';

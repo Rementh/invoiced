9244644028f0e699ca3de07af05d5397be7b6746
Filip <talaga.filip@gmail.com>
2020-03-15 18:56:06 +0100

    Added "last" array prototype function

--- src/utils/array-prototypes.ts
+++ src/utils/array-prototypes.ts
@@ -1,7 +1,12 @@
 interface Array<T> {
     sum: () => number;
+    last: () => T;
 }
 
 Array.prototype.sum = function() {
     return this.reduce((prev: number, curr: number) => prev + curr, 0);
+};
+
+Array.prototype.last = function() {
+    return this[this.length - 1];
 };
